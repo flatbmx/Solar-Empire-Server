2bf7ea03bd3f59d78218ee1f7027cecdbe7b8c2d
flatbmx <david@podts.com>
2013-07-04 11:40:46 -0500

    Error handling in network manager.

--- src/com/podts/solarserver/network/NetworkManager.java
+++ src/com/podts/solarserver/network/NetworkManager.java
@@ -4,13 +4,17 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.logging.Logger;
 
 import com.podts.solarserver.Server;
+import com.podts.solarserver.console.GameLogger;
 import com.podts.solarserver.entity.UnVerifyedPlayer;
+import com.podts.solarserver.interfaces.Consolable;
 
-public class NetworkManager implements Runnable {
+public class NetworkManager implements Runnable, Consolable {
 	
 	private ServerSocket listensocket;
+	private Logger logger = new GameLogger("Network");
 	
 	public void run() {
 		
@@ -29,13 +33,19 @@ public class NetworkManager implements Runnable {
 		
 	}
 	
+	@Override
+	public Logger getLogger() {
+		return logger;
+	}
+	
 	public NetworkManager() {
 		try {
 			listensocket = new ServerSocket(5000);
 			listensocket.setSoTimeout(100);
 			new Thread(this).start();
 		} catch (IOException e) {
-			System.out.println("error");
+			e.printStackTrace();
+			getLogger().warning("Error initializing network manager.");
 		}
 	}
 	
